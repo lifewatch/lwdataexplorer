9bf2c1a2e9032631dfeed841462e72e8c7113438
Salvador Fernandez <salvadorf.fernandez@vliz.be>
2021-11-30 14:15:46 +0100

    Implement logic to query from lwdataserver in all data types and improve qc

--- tests/testthat/test-station.R
+++ tests/testthat/test-station.R
@@ -9,10 +9,10 @@ station_df <- getStationData("2019-07-22", "2019-07-23", station_stations, stati
 # Test Station
 test_that("Station", {
   #skip_on()
-  expect_equal(class(station_list), "list")
-  expect_equal(class(station_list$par), "list")
-  expect_equal(class(station_list$mdf), 'data.frame')
-  expect_equal(class(station_df), 'data.frame')
+  expect_true(is.list(station_list))
+  expect_true(is.list(station_list$par))
+  expect_true(is.data.frame(station_list$mdf))
+  expect_true(is.data.frame(station_df))
   expect_warning(
     getStationData('9999-12-01', '9999-12-31', station_stations, station_categories)
   )
