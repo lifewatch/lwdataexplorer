c0d0a05d0afef01feb6dc9657e1f960a2f050ac0
Dias <dias.bakeev@vliz.be>
2026-04-08 17:16:22 +0200

    add some tests

--- tests/testthat/test-cpod.R
+++ tests/testthat/test-cpod.R
@@ -16,3 +16,34 @@ test_that("CPOD", {
     getCpodData('9999-12-01', '9999-12-31', quality = cpod_quality)
   )
 })
+
+test_that("getCpodData returns list with correct names when params = TRUE", {
+  data <- getCpodData("2020-04-20", "2020-04-21", quality = "Hi", params = TRUE)
+  expect_equal(names(data), c("mdf", "par"))
+  expect_equal(unique(data$mdf$quality), 3)
+})
+
+test_that("getCpodData filters species and quality correctly", {
+  data <- getCpodData("2020-04-20", "2020-04-21", species = c("Dolphins", "NBHF"), quality = c("Mod"))
+  expect_setequal(unique(data$species), c("NBHF", "Dolphins"))
+  expect_equal(unique(data$quality), 2)
+})
+
+test_that("getCpodData filters single species correctly", {
+  data <- getCpodData("2020-04-20", "2020-04-21", species = "Dolphins")
+  expect_equal(unique(data$species), "Dolphins")
+  expect_equal(unique(data$quality), 3)
+})
+
+test_that("getCpodData applies projectcode", {
+  expect_warning(getCpodData("2020-04-20", "2020-04-21", projectcode = "nonexistent"), "No data returned")
+})
+
+test_that("getCpodData validate parameters", {
+  expect_warning(getCpodData("2020-04-20", "2020-04-21", projectcode = ";notallowed"), "Something unexpected happened. Check the server logs.") #TODO: better message
+})
+
+test_that("getCpodData with multiple quality filters returns correct quality codes", {
+  data <- getCpodData("2020-04-20", "2020-04-21", quality = c("Hi", "Mod"))
+  expect_setequal(unique(data$quality), c(2, 3)) #TODO: fails
+})
